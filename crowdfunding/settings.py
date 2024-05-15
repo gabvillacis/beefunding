cb537683c623602f1be7e323c80f0af7956f3a8c
Gabriel Villacis <55613545+gabvillacis@users.noreply.github.com>
2024-05-14 22:55:37 -0500

    Update settings.py

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -15,16 +15,11 @@ from django.conf.locale.es import formats as es_formats
 import os
 import environ
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
